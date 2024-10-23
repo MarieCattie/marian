50df3eb59ce3cf29035cbcca8e00eaa0964ceee1
Maria <mashalena123@mail.ru>
2024-10-23 10:28:23 +0300

    add new project monolit

---
+++ layout/monolit/assets/js/custom.js
@@ -0,0 +1,14 @@
+/*
+You can use this file with your scripts.
+It will not be overwritten when you upgrade solution.
+*/
+
+$(document).ready(function(){
+
+
+	$('.contact-tel').hover(function(){
+
+		$(this).toggleClass('contact-tel-red')
+	})
+
+});
